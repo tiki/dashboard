cd5ccbeb0e96c5719a372549d52ee1fe248ef52b
Miro Benicio <mirodantas27@gmail.com>
2024-06-27 16:41:32 -0300

    wip:navigation guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '../LoginPage/LoginPage.vue'
+import HomePage from '../HomePage/HomePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage
+    },
     {
       path: '/login',
       name: 'login',
