0da45ad57aa8b83f9b2950e84639dcbd62070237
Miro Benicio <mirodantas27@gmail.com>
2024-08-05 09:46:39 -0300

    feat: save token locally

--- src/app/src/App.vue
+++ src/app/src/App.vue
@@ -1,6 +1,21 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import MainLayout from './MainLayout/MainLayout.vue'
+import { onMounted } from 'vue'
+import { setCookie } from '@/CookieService/index'
+
+onMounted(() => {
+  const queryString = window.location.search
+  const urlParams = new URLSearchParams(queryString)
+
+  const idToken = urlParams.get('id_token')
+  const accessToken = urlParams.get('access_token')
+
+  if (!idToken || !accessToken) return (window.location.href = 'https://github.com')
+
+  setCookie('id_token', idToken!, 1)
+  setCookie('access_token', accessToken!, 1)
+})
 </script>
 
 <template>
--- src/app/src/router/index.ts
+++ src/app/src/router/index.ts
@@ -5,6 +5,7 @@ import NewRequestPage from '../NewRequestPage/NewRequestPage.vue'
 import RequestsPage from '../RequestsPage/RequestsPage.vue'
 import ThreadPage from '../ThreadPage/ThreadPage.vue'
 import DatasetsPage from '../DatasetsPage/DatasetsPage.vue'
+import { getCookie } from '@/CookieService/index'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -55,11 +56,12 @@ const router = createRouter({
   ]
 })
 
-router.beforeEach(async (to, from) => {
-  const token = sessionStorage.getItem('token')
-  if (!token && to.name !== 'login') {
-    return { name: 'login' }
+router.beforeEach((to, from, next) => {
+  const token = getCookie('access_token')
+  if (!token && to.name !== 'home') {
+    return (window.location.href = 'https://github.com')
   }
+  next()
 })
 
 export default router
