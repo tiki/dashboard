3095898516d819db4bd688d45df1f4c6c0fca0c7
Miro Benicio <mirodantas27@gmail.com>
2024-09-03 11:25:09 -0300

    fix: font-family wip: lagoon table

--- src/app/src/pages/data-publishing/Lagoon/LagoonPage.vue
+++ src/app/src/pages/data-publishing/Lagoon/LagoonPage.vue
@@ -1,10 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LagoonTable from './LagoonTable.vue'
+</script>
 
 <template>
   <div class="ml-[12rem] flex justify-start items-between h-screen flex-col gap-4 py-10 px-16">
     <div class="flex justify-between items-center px-3">
       <h1 class="text-3xl uppercase text-dark-blue font-bold">Lagoon</h1>
     </div>
+    <LagoonTable />
   </div>
 </template>
 
