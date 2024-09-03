5a218f6cfaaa396311573f119651e933823497c3
Miro Benicio <mirodantas27@gmail.com>
2024-09-03 11:46:42 -0300

    wip: deploy lagoon button

--- src/app/src/pages/data-publishing/Lagoon/LagoonPage.vue
+++ src/app/src/pages/data-publishing/Lagoon/LagoonPage.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import LagoonTable from './LagoonTable.vue'
+import Button from 'primevue/button'
 </script>
 
 <template>
   <div class="ml-[12rem] flex justify-start items-between h-screen flex-col gap-4 py-10 px-16">
     <div class="flex justify-between items-center px-3">
       <h1 class="text-3xl uppercase text-dark-blue font-bold">Lagoon</h1>
+      <Button label="Deploy" style="background-color: #00b272" @click="isVisible = !isVisible" />
     </div>
     <LagoonTable />
   </div>
